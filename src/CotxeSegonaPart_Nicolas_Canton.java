0e852a3405c4360e003bb14c533c658a6a8dc725
NicolasCanton <ncanton@nigul.cide.es>
2020-12-11 19:13:44 +0100

    test

--- src/CotxeSegonaPart_Nicolas_Canton.java
+++ src/CotxeSegonaPart_Nicolas_Canton.java
@@ -1,16 +1,2 @@
-public class CotxeSegonaPart_Nicolas_Canton extends TestCotxe_Nicolas_Canton {
-    public CotxeSegonaPart_Nicolas_Canton(String marca ,String model ,TipusCanvi tipuscanvi) {
-        super(marca ,model ,tipuscanvi);
-    }
-
-    public TipusCanvi CanviMarxa() {
-        if (tipuscanvi == TipusCanvi.CanviAutomatic) {
-            System.out.println("El cambio de marchas es automático");
-        } else
-            System.out.println("El cambio de marchas es manual.");
-        return tipuscanvi;
-    }
-
-}
 
 
